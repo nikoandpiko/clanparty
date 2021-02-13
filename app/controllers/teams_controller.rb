ee29481da3d94bfe52b7d8f356eaec4a245d07ce
Lawrence Kroll <larry43.lk@gmail.com>
2021-02-13 16:03:01 +0900

    teams new/create added.protected key in .env

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,4 +15,32 @@ class TeamsController < ApplicationController
     @team = Team.new
     authorize @team
   end
+
+  def create
+    team_params[:game_id] == "" ? team_params[:game_id] = nil :
+
+      @game = Game.find(team_params[:game_id])
+    @team = Team.new(
+      game: @game,
+      user: current_user,
+      name: team_params[:name],
+      status: team_params[:status],
+      discord: team_params[:discord],
+      bio: team_params[:bio],
+      avatar: team_params[:avatar]
+    )
+    authorize @team
+
+    if @team.save
+      redirect_to teams_path, notice: "Team successfully created"
+    else
+      render :new, notice: "Please fill in the necessary information"
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:game_id, :name, :status, :discord, :bio, :avatar)
+  end
 end
