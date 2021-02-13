31b0d776cad61284d1fc4631edc8ebc4fcb83563
Lawrence Kroll <larry43.lk@gmail.com>
2021-02-13 14:10:19 +0900

    dry seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,50 +8,65 @@
 
 puts "Cleaning up database"
 
-User.destroy_all
-Game.destroy_all
-Team.destroy_all
 Event.destroy_all
+Team.destroy_all
+Game.destroy_all
+User.destroy_all
 Invite.destroy_all
 puts "All clean"
 
-role = ["Tank", "Healer", "DPS"] # 2 tanks, 2 healers, 4 DPS per team max
-hours = (10..23)
-half_hour = [3, 0]
-start_time_hour = rand(hours)
-end_time_hour = rand(hours)
-if start_time_hour < end_time_hour
-  start_time = "#{start_time_hour}:#{half_hour.sample}0:00"
-  end_time = "#{end_time_hour}:#{half_hour.sample}0:00"
-end
+
 # Users
+puts "Creating random times"
 puts "Creating users"
-# 10.times do
-User.create(
-  username: Faker::Games::Witcher.monster,
-  email: "1@1.com",
-  password: "123456",
-  avatar: Faker::Avatar.image,
-  nickname: Faker::Games::ElderScrolls.first_name,
-  bio: Faker::Quote.matz,
-  discord: "https://discord.gg/thisshouldnotworkhopefully",
-  role: role.sample,
-  sunday_start: start_time,
-  sunday_end: end_time,
-  monday_start: start_time,
-  monday_end: end_time,
-  tuesday_start: start_time,
-  tuesday_end: end_time,
-  wednesday_start: start_time,
-  wednesday_end: end_time,
-  thursday_start: start_time,
-  thursday_end: end_time,
-  friday_start: start_time,
-  friday_end: end_time,
-  saturday_start: start_time,
-  saturday_end: end_time
-)
-# end
+n = 0
+role = ["Tank", "Healer", "DPS"] # 2 tanks, 2 healers, 4 DPS per team max
+def random_start_end_time(start_time, end_time)
+  array = []
+  half_hour = [3, 0]
+  if start_time < end_time
+    array << "#{start_time}:#{half_hour.sample}0:00"
+    array << "#{end_time}:#{half_hour.sample}0:00"
+    return array
+  else
+    array << "#{end_time}:#{half_hour.sample}0:00"
+    array << "#{start_time}:#{half_hour.sample}0:00"
+    return array
+  end
+end
+80.times do
+  # valid start and end time
+  # each weekday start and end time
+  days_times = []
+  7.times do
+    days_times << random_start_end_time(rand(10..23), rand(10..23))
+  end
+
+  User.create(
+    username: Faker::Games::Witcher.monster,
+    email: "#{n+=1}@#{n+=1}.com",
+    password: "123456",
+    avatar: Faker::Avatar.image,
+    nickname: Faker::Games::ElderScrolls.first_name,
+    bio: Faker::Quote.matz,
+    discord: "https://discord.gg/thisshouldnotworkhopefully",
+    role: role.sample,
+    sunday_start: days_times[0][0],
+    sunday_end: days_times[0][1],
+    monday_start: days_times[1][0],
+    monday_end: days_times[1][1],
+    tuesday_start: days_times[2][0],
+    tuesday_end: days_times[2][1],
+    wednesday_start: days_times[3][0],
+    wednesday_end: days_times[3][1],
+    thursday_start: days_times[4][0],
+    thursday_end: days_times[4][1],
+    friday_start: days_times[5][0],
+    friday_end: days_times[5][1],
+    saturday_start: days_times[6][0],
+    saturday_end: days_times[6][1]
+  )
+end
 # Games only FFXIV
 puts "Creating Game"
 content = ["The Sirensong Sea", "Amaurot", "The Dying Gasp", "Anamnesis Anyder", "Mt. Gulg"]
@@ -63,27 +78,34 @@ Game.create(
 
 # Teams
 puts "Creating Teams"
-status = ["Open", "Closed"]
-Team.create(
-  name: Faker::Games::StreetFighter.stage,
-  avatar: Faker::Avatar.image,
-  bio: Faker::Games::StreetFighter.quote,
-  discord: "https://discord.gg/thisshouldnotworkhopefullyteams",
-  status: status.sample,
-  user_id: User.last.id,
-  game_id: Game.last.id
-)
+users_for_seed = User.all
+10.times do
+  status = [0, 1] # 0 is closed, 1 is open
+  Team.create(
+    name: Faker::Games::StreetFighter.stage,
+    avatar: Faker::Avatar.image,
+    bio: Faker::Games::StreetFighter.quote,
+    discord: "https://discord.gg/thisshouldnotworkhopefullyteams",
+    status: status.sample,
+    user_id: users_for_seed.sample.id,
+    game_id: Game.last.id
+  )
+end
 
 # Events
 puts "Creating Events"
-day = 1..7
-Event.create(
-  name: Faker::Games::Fallout.quote,
-  description: Faker::Games::Overwatch.quote,
-  day: rand(day),
-  start_time: start_time,
-  end_time: end_time,
-  team_id: Team.last.id
-)
-
+50.times do
+  days_times_events = []
+  days_times_events << random_start_end_time(rand(10..23), rand(10..23))
+  day = 1..7
+  teams_for_seed = Team.all
+  Event.create(
+    name: Faker::Games::Fallout.quote,
+    description: Faker::Games::Overwatch.quote,
+    day: rand(day),
+    start_time: days_times_events[0][0],
+    end_time: days_times_events[0][1],
+    team_id: teams_for_seed.sample.id
+  )
+end
 # Applications no seed
