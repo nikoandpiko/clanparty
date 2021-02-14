675720a982c156016db0f45fd7b39cafb1c50bb4
David Smith <smitty6000@gmail.com>
2021-02-14 20:54:34 +0900

    hover only activates if already clicked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :bio, :dicord, :role)
+    params.require(:user).permit(:nickname, :bio, :discord, :role)
   end
 
 end
