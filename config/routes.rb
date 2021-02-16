00f7be1005e3d3242bc4cb763865fd5f401a5f8e
Nick Roma <nikoandpiko@gmail.com>
2021-02-16 22:04:18 +0900

    Updates layout for index, changes root to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'teams#index'
+  # root to: 'teams#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :teams, except: [ :edit, :update, :delete ] do
     resources :invites, except: [ :delete, :update, :edit ]
