28ae093b6551ff3d0c06f8c3a59cb9146a40d34b
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-04 22:34:45 +0900

    removed discordjs

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,9 +1,9 @@
-require 'discordrb'
+# require 'discordrb'
 
-$bot = Discordrb::Bot.new token: ENV["DISCORD_KEY"]
+# $bot = Discordrb::Bot.new token: ENV["DISCORD_KEY"]
 
-$bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
-end
+# $bot.message(with_text: 'Ping!') do |event|
+#   event.respond 'Pong!'
+# end
 
 # $bot.run
