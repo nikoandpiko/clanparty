8fec5bc7d0d7f037d7264c6f9f03fcccdc2af000
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-06 11:40:08 +0900

    customizing event msg on discord

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,11 +14,11 @@ class EventsController < ApplicationController
 
     if @event.save
       sleep(1.5)
-      sent_event_discord(params[:name])
+      # raise
+      sent_event_discord(@event.name, @event.description, @event.day, @event.start_time, @event.end_time, @team)
       redirect_to team_path(@team)
     else
       redirect_to team_path(@team), notice: "Please enter all information"
-      raise
     end
   end
 
@@ -31,17 +31,20 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:name, :description, :day, :start_time, :end_time)
   end
+  # WEBHOOK_URL = ENV["DISCORD_URL"]
   WEBHOOK_URL = 'https://discord.com/api/webhooks/817030712401330176/_rXms5NGitxqwPbyoBXu-teAcYi3zZJN7TrDDAlzWLe_UJyJZy3mDTTJ34nwaYQLOllX'.freeze
 
-  def sent_event_discord(name)
+  def sent_event_discord(name, description, day, start_time, end_time, team)
     require 'discordrb/webhooks'
 
     client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
     client.execute do |builder|
-      builder.content = '@here NEW Event!'
+      builder.content = '"@"here NEW Event!'
       builder.add_embed do |embed|
-        embed.title = name
-        embed.description = 'Raid until your eyes start to bleed!'
+        embed.title = "[#{name}](https://clanparty.herokuapp.com/teams#{team.id}/)"
+        # change LINK to clanparty.net later!!!
+        embed.description = "#{description} from #{start_time} till #{end_time}. AND day is #{day}"
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: Event::EVENT_IMAGE[name])
         embed.timestamp = Time.now
       end
     end
