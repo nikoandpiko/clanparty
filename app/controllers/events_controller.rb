16ce2b40de95da07504a3fa3894bb702c38dce5c
Lawrence Kroll <larry43.lk@gmail.com>
2021-03-06 09:54:44 +0900

    new controller action for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
 
     if @event.save
       sleep(1.5)
+      sent_event_discord(params[:name])
       redirect_to team_path(@team)
     else
       redirect_to team_path(@team), notice: "Please enter all information"
@@ -29,5 +30,20 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:name, :description, :day, :start_time, :end_time)
+  end
+  WEBHOOK_URL = 'https://discord.com/api/webhooks/817030712401330176/_rXms5NGitxqwPbyoBXu-teAcYi3zZJN7TrDDAlzWLe_UJyJZy3mDTTJ34nwaYQLOllX'.freeze
+
+  def sent_event_discord(name)
+    require 'discordrb/webhooks'
+
+    client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+    client.execute do |builder|
+      builder.content = '@here NEW Event!'
+      builder.add_embed do |embed|
+        embed.title = name
+        embed.description = 'Raid until your eyes start to bleed!'
+        embed.timestamp = Time.now
+      end
+    end
   end
 end
