24327be9a7be5b912012f0d801c194f61075c384
David Smith <smitty6000@gmail.com>
2021-03-08 17:07:27 +0900

    declaring schedules in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,33 +5,78 @@ class TeamsController < ApplicationController
     @teams = policy_scope(Team)
     @users = User.all
 
+    counter = 0
     @each_teams_members = []
+    until counter >= @teams.length do
+      @each_teams_members[counter] = []
+      counter += 1
+    end
+    
+    counter2 = 0
     @teams.each do |team|
       @accepted_members = Invite.accepted.where(team_id: team.id)
       @team_leader = User.where(team_id: team.id)
       @accepted_members.each do |invite|
         @user = invite.user
-        @each_teams_members << @user
-        
+        @each_teams_members[counter2] << @user
+      end   
+      @each_teams_members[counter2] << @team_leader[0]
+      counter2 += 1
+    end   
+
+    @monday_s = []
+    @monday_e = []
+    @tuesday_s = []
+    @tuesday_e = []
+    @wednesday_s = []
+    @wednesday_e = []
+    @thursday_s = []
+    @thursday_e = []
+    @friday_s = []
+    @friday_e = []
+    @saturday_s = []
+    @saturday_e = []
+    @sunday_s = []
+    @sunday_e = []
+    
+    counter3 = 0
+    until counter3 >= @each_teams_members.length
+      @monday_s[counter3] = []
+      @monday_e[counter3] = []
+      @tuesday_s[counter3] = []
+      @tuesday_e[counter3] = []
+      @wednesday_s[counter3] = []
+      @wednesday_e[counter3] = []
+      @thursday_s[counter3] = []
+      @thursday_e[counter3] = []
+      @friday_s[counter3] = []
+      @friday_e[counter3] = []
+      @saturday_s[counter3] = []
+      @saturday_e[counter3] = []
+      @sunday_s[counter3] = []
+      @sunday_e[counter3] = []
+
+      @each_teams_members[counter3].each do |member|
+        @monday_s[counter3] << member.monday_start
+        @monday_e[counter3] << member.monday_end
+        @tuesday_s[counter3] << member.tuesday_start
+        @tuesday_e[counter3] << member.tuesday_end
+        @wednesday_s[counter3] << member.wednesday_start
+        @wednesday_e[counter3] << member.wednesday_end
+        @thursday_s[counter3] << member.thursday_start
+        @thursday_e[counter3] << member.thursday_end
+        @friday_s[counter3] << member.friday_start
+        @friday_e[counter3] << member.friday_end
+        @saturday_s[counter3] << member.saturday_start
+        @saturday_e[counter3] << member.saturday_end
+        @sunday_s[counter3] << member.sunday_start
+        @sunday_e[counter3] << member.sunday_end
       end
-      @each_teams_members << @team_leader[0]
+      counter3 += 1
     end
-
-    # @each_teams_members.each_with_index do |member, index|
-    #   if !member.team_id.nil?
-    #     @each_teams_members.slice(", ")
-    #   end
-
- raise
-   
-    end
+    raise
 
 # NEED TO SPLIT MEMBERS INTO SEPARATE ELEMENTS
-
-
-   
-
-
   end
 
   def show
