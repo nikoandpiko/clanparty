f5d034f21d72f0909f7f138582ba6f408c913051
Nick Roma <nikoandpiko@gmail.com>
2021-03-24 02:38:23 +0900

    Allows for user and team creation without details

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
+  end
   # root to: 'teams#index'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :teams, except: [ :delete ] do
-    resources :invites, except: [ :delete, :update, :edit ]
-    resources :events,  only: [ :new, :create ]
+  resources :teams, except: [:delete] do
+    resources :invites, except: %i[delete update edit]
+    resources :events,  only: %i[new create]
     # resources :users, only: [ :delete ]
   end
 
-  resources :users, except: [ :delete ] do
-    resources :invites, except: [ :delete, :update, :edit ]
+  resources :users, except: [:delete] do
+    resources :invites, except: %i[delete update edit]
   end
 
-  get '/users/:id/edit_schedule' =>'users#edit_schedule', as: :edit_schedule
-  patch '/users/:id/edit_schedule'=>'users#edit_schedule'
+  get '/users/:id/edit_schedule' => 'users#edit_schedule', as: :edit_schedule
+  patch '/users/:id/edit_schedule' => 'users#edit_schedule'
 
-  resources :invites, only: [ :edit, :update ]
+  resources :invites, only: %i[edit update]
 end
